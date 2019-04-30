11531281dd5317dd604c72ba2386b50dcc552bd0
Mugh Dzin Syah <mughdzinsyah@Mughs-Air.local>
2019-04-30 10:40:06 +0700

    edit2

--- components/menu.vue
+++ components/menu.vue
@@ -19,7 +19,7 @@
           </v-list-tile-avatar>
 
           <v-list-tile-content>
-            <v-list-tile-title>John Leider</v-list-tile-title>
+            <v-list-tile-title>Jono</v-list-tile-title>
           </v-list-tile-content>
         </v-list-tile>
       </v-list>
